4fbfe700fb0edb9236d146591c284bc5c25e4e39
sushanslondhe <ballylondhe20@gmail.com>
2024-08-11 15:06:18 +0530

    webhook logic added

---
+++ hooks/src/index.ts
@@ -0,0 +1,33 @@
+import Express from "express";
+import { PrismaClient } from "@prisma/client";
+
+const client = new PrismaClient();
+const app = Express();
+app.use(Express.json());
+
+app.get("/hooks/catch/:userId/:zapId", async (req, res) => {
+  const userId = req.params.userId;
+  const zapId = req.params.zapId;
+  const body = req.body;
+
+  await client.$transaction(async (tx) => {
+    const run = await tx.zapRun.create({
+      data: {
+        zapId,
+        metadata: body,
+      },
+    });
+
+    await tx.zapRunOutbox.create({
+      data: {
+        zapRunId: run.id,
+      },
+    });
+  });
+
+  res.json({
+    message: "webhook received",
+  });
+});
+
+app.listen(3001);
